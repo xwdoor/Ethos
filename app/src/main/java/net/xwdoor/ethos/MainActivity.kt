1e3225fe9da846d621e1b7cab06a7d9eb39c6e75
xiaowei <xiaowei@ling.ai>
2018-05-12 19:19:50 +0800

    open one world

---
+++ app/src/main/java/net/xwdoor/ethos/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.xwdoor.ethos
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
